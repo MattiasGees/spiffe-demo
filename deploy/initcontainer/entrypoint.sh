86c9eeb0f3bb247184c0251008505379d9c9eaff
Mattias Gees <mattias.gees@cyberark.com>
2025-02-05 15:37:19 +0000

    Fix typo

--- deploy/initcontainer/entrypoint.sh
+++ deploy/initcontainer/entrypoint.sh
@@ -7,7 +7,7 @@ mkdir -p /tmp/aws
 if [ "$AWS_AUTH" == "X509" ]; then
 cat > /tmp/aws/config <<EOL
 [default]
-credential_process = /usr/bin/aws-spiffe-workload-helper x509-credential-process --profile-arn ${AWS_PROFILE_ARN} --trust-anchor-arn ${AWS_TRUSTANCHOR_ARN} --role-arn ${AWS_ROLE_ARN}
+credential_process = /usr/bin/aws-spiffe-workload-helper x509-credential-process --profile-arn ${AWS_PROFILE_ARN} --trust-anchor-arn ${AWS_TRUST_ANCHOR_ARN} --role-arn ${AWS_ROLE_ARN}
 EOL
 else
 cat > /tmp/aws/config <<EOL
